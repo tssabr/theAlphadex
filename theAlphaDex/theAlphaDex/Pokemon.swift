c0db4923c612a0996d6779bc34bb3b55b36bc75e
IamTinyTurtle <tss.abr@gmail.com>
2017-01-03 18:23:15 -0500

    Finished Alphadex
    However has buggy constraints causing data to not properly load.

--- theAlphaDex/theAlphaDex/Pokemon.swift
+++ theAlphaDex/theAlphaDex/Pokemon.swift
@@ -21,10 +21,49 @@ class Pokemon {
     private var _weight: String!
     private var _attack: String!
     private var _evolution: String!
+    private var _nextEvolutionTxt: String!
+    private var _nextEvolutionName: String!
+    private var _nextEvolutionId: String!
+    private var _nextEvolutionLevel: String!
     private var _pokemonURL: String!
     
+    //Getters
+    var nextEvolutionLevel: String {
+        
+        if _nextEvolutionLevel == nil {
+            
+            _nextEvolutionLevel = ""
+        }
+        return _nextEvolutionLevel
+    }
     
-    //getters
+    var nextEvolutionId: String {
+        
+        if _nextEvolutionId == nil {
+            
+            _nextEvolutionId = ""
+        }
+        return _nextEvolutionId
+    }
+    
+    var nextEvolutionName: String {
+        
+        if _nextEvolutionName == nil {
+            
+            _nextEvolutionName = ""
+        }
+        return _nextEvolutionName
+    }
+    
+    var nextEvolutionText: String {
+        
+        if _nextEvolutionTxt == nil {
+            
+            _nextEvolutionTxt = ""
+        }
+        return _nextEvolutionTxt
+    }
+    
     var name: String {
         return _name
     }
@@ -35,51 +74,44 @@ class Pokemon {
     
     var description: String {
         if _description == nil {
-            _description = ""
+            _description = "_"
         }
         return _description
     }
     
     var type: String {
         if _type == nil {
-            _type = ""
+            _type = "_"
         }
         return _type
     }
     
     var defense: String {
         if _defense == nil {
-            _defense = ""
+            _defense = "_"
         }
         return _defense
     }
     
     var height: String {
         if _height == nil {
-            _height = ""
+            _height = "_"
         }
         return _height
     }
     
     var weight: String {
         if _weight == nil {
-            _weight = ""
+            _weight = "_"
         }
         return _weight
     }
     
     var attack: String {
         if _attack == nil {
-            _attack = ""
+            _attack = "_"
         }
         return _attack
-    }
-    
-    var nextEvolutionText: String {
-        if _evolution == nil {
-            _evolution = ""
-        }
-        return _evolution
     }
     
     init(name: String, pokedexId: Int) {
@@ -87,7 +119,7 @@ class Pokemon {
         self._name = name
         self._pokedexId = pokedexId
         
-        self._pokemonURL = "\(URL_BASE)\(URL_POKEMON)\(pokedexId)/"
+        self._pokemonURL = "\(URL_BASE)\(URL_POKEMON)\(self._pokedexId)/"
     }
     
     func downloadPokemonDetails(completed: @escaping DownloadComplete) {
@@ -105,7 +137,71 @@ class Pokemon {
                 if let defense = dict["defense"] as? Int {
                     self._defense = "\(defense)"
                 }
+                
+                if let types = dict["types"] as? [Dictionary<String, String>] , types.count > 0 {
+                    if let name = types[0]["name"] {
+                        self._type = name.capitalized
+                    }
+                    
+                    if types.count > 1 {
+                        for x in 1..<types.count {
+                            if let name = types[x]["name"] {
+                                self._type! += "/\(name.capitalized)"
+                            }
+                        }
+                    }
+                } else {
+                    self._type = ""
+                }
+                
+                if let descArr = dict["description"] as? [Dictionary<String, String>] , descArr.count > 0 {
+                    if let url = descArr[0]["resource_uri"] {
+                        let descUrl = "\(URL_BASE)\(url)"
+                        Alamofire.request(descUrl).responseJSON { response in
+                            if let descDict = response.result.value as? Dictionary<String, AnyObject> {
+                                if let description = descDict["descriptions"] as? String {
+                                    
+                                    let newDescription = description.replacingOccurrences(of: "POKMON", with: "Pokemon")
+                                    self._description = newDescription
+                                    print(newDescription)
+                                }
+                            }
+                            completed()
+                        }
+                    }
+                } else {
+                    self._description = ""
+                }
+                
+                if let evolutions = dict["evolutions"] as? [Dictionary<String, AnyObject>] , evolutions.count > 0 {
+                    if let nextEvolution = evolutions[0]["to"] as? String {
+                        
+                        if nextEvolution.range(of: "mega") == nil {
+                            self._nextEvolutionName = nextEvolution
+                            
+                            if let uri = evolutions[0]["resource_uri"] as? String {
+                                let newStr = uri.replacingOccurrences(of: "/api/v1/pokemon/", with: "")
+                                let nextEvoId = newStr.replacingOccurrences(of: "/", with: "")
+                                self._nextEvolutionId = nextEvoId
+                                
+                                if let lvlExist = evolutions[0]["level"] {
+                                    if let lvl = lvlExist as? Int {
+                                        self._nextEvolutionLevel = "\(lvl)"
+                                    }
+                                    
+                                } else {
+                                    self._nextEvolutionLevel = ""
+                                }
+                            }
+                        }
+                    }
+                    
+                    print(self.nextEvolutionLevel)
+                    print(self.nextEvolutionName)
+                    print(self.nextEvolutionId)
+                }
             }
+            completed()
         }
     }
 }
--- theAlphaDex/theAlphaDex/PokemonDetailVC.swift
+++ theAlphaDex/theAlphaDex/PokemonDetailVC.swift
@@ -28,16 +28,42 @@ class PokemonDetailVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        nameLbl.text = pokemon.name
+        let img = UIImage(named: "\(pokemon.pokedexId)")
+        
+        mainImg.image = img
+        currentEvoImg.image = img
+        pokedexIdLbl.text = "\(pokemon.pokedexId)"
         pokemon.downloadPokemonDetails {
             //will only be called after its finished
+            print("did make it here")
             self.updateUI()
         }
 
-        //nameLbl.text = pokemon.name
+        
+        
     }
     
     func updateUI() {
         
+        attackLbl.text = pokemon.attack
+        defenseLbl.text = pokemon.defense
+        heightLbl.text = pokemon.height
+        weightLbl.text = pokemon.weight
+        typeLbl.text = pokemon.type
+        descriptionLbl.text = pokemon.description
+        
+        if pokemon.nextEvolutionId == "" {
+            evolutionLbl.text = "No Evolutions"
+            nextEvoImg.isHidden = true
+        } else {
+            nextEvoImg.image = UIImage(named: pokemon.nextEvolutionId)
+            nextEvoImg.isHidden = false
+            
+            let str = "Next Evolution: \(pokemon.nextEvolutionName) - LVL \(pokemon.nextEvolutionLevel)"
+            evolutionLbl.text = str
+        
+        }
     }
     
     @IBAction func backButtonPressed(_ sender: UIButton) {
